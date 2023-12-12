ca3881a7cbb752fb88a54c1490b7eb66880df7c2
intissar12345 <302834832@student.rocmondriaan.nl>
2023-12-12 15:24:32 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 console.log("het werkt");
 
-let blocks = document.querySelectorAll(".box");
+// let blocks = document.querySelectorAll(".box");
+
+tiles = document.querySelector(".box");
+tiles.addEventListener("click", function () {
+  console.log("Hi");
+    // let test2 = document.querySelector(".div2")
+    // test2.innerHTML = "<p>X</p>"
+})
